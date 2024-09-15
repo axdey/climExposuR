957c76009c260cf28db2582041bd38af1bd9530e
axdey <arnabxdey@gmail.com>
2024-09-15 16:05:29 -0700

    updated code to extract grid-data users can select range of files + handle nic files with no time attr

--- R/1.1-extract-clim-data-gridded-nics.R
+++ R/1.1-extract-clim-data-gridded-nics.R
@@ -6,8 +6,11 @@
 #' @param path_nic_files Path to the NetCDF files
 #' @param sf_file Shape file
 #' @param sf_file_admin Administrative variable in the shape file
+#' @param reference_date Reference date for the climate data
+#' @param file_frequency Frequency of the climate data
+#' @param start_index Start index of the NetCDF files
+#' @param end_index End index of the NetCDF files
 #' @return A data frame with the extracted climate data
-#' @examples see below
 #' @importFrom terra vect rast ext rotate crop extract
 #' @importFrom sf st_transform
 #' @importFrom ncdf4 nc_open ncvar_get ncatt_get nc_close
@@ -17,117 +20,84 @@
 
 func_extract_clim_data_shp <- function(path_nic_files, 
                                        sf_file, 
-                                       sf_file_admin) {
+                                       sf_file_admin,
+                                       reference_date = "1900-01-01", 
+                                       file_frequency = "daily",
+                                       start_index = 1,
+                                       end_index = NULL) {
   # Step-1: Process sf file and create a SpatVector object
-  ## Convert CRS
   sf_file_wgs84 <- st_transform(sf_file, crs = 4326)
-  ## Convert sf object to SpatVector
   sf_file_sv <- vect(sf_file_wgs84)
   
   # Step-2: Get the list of nic files
   clim_files <- list.files(path = path_nic_files, pattern = "\\.nc$", full.names = TRUE)
+  if (is.null(end_index)) end_index <- length(clim_files)
+  clim_files <- clim_files[start_index:end_index]
 
-  # Step-3: Initialize an empty data frame to store the results
-  results_df <- data.frame()
+  # Step-3: Initialize an empty list to store the results
+  results_list <- list()
 
   # Function to process a single NC file
   process_nc_file <- function(nc_file_path) {
-    # Read the NC file as a SpatRaster
     clim_data_rast <- rast(nc_file_path)
-    # compare the extent of the raster and the sf file and rotate the raster if necessary
     if ((ext(clim_data_rast)$xmin > ext(sf_file_sv[1,])$xmin) | (ext(clim_data_rast)$xmax < ext(sf_file_sv[1,])$xmax)) {
       clim_data_rast <- rotate(clim_data_rast)
     }
     
+    file_results <- list()
+    
     for (i in 1:nrow(sf_file_sv)) {
-      # Extract the user-defined attribute
       area_attribute <- sf_file_sv[[sf_file_admin]][,1][i]
-      # Extract the area of interest from the climate data
-      ext(clim_data_rast)
-      ext(sf_file_sv[i, ])
       area_clim_data_rast <- crop(clim_data_rast, sf_file_sv[i, ])
       area_mean_temp <- terra::extract(area_clim_data_rast, sf_file_sv[i, ], fun = mean, na.rm = TRUE)
+      clim_daily_mean <- sapply(area_mean_temp[-1], mean, na.rm = TRUE)
 
-      # Assuming climate data is daily, compile the mean climate for each day
-      clim_daily_mean <- sapply(area_mean_temp, mean, na.rm = TRUE)
-      # Drop the first column which is the ID
-      clim_daily_mean <- clim_daily_mean[-1]
-
-      # Convert to a data frame and add necessary columns
-      clim_df <- as.data.frame(clim_daily_mean)
-      clim_df$Attribute <- area_attribute
-      
-      # Get start and end dates from the nc file
-      nc_data <- nc_open(nc_file_path)
-      time_var <- ncvar_get(nc_data, "time")
-      time_units <- ncatt_get(nc_data, "time", "units")$value
-      ref_date_str <- sub(".*since ", "", time_units)
-      unit <- strsplit(time_units, " ")[[1]][1]
-      
-      if(grepl("days", time_units)) {
-        ref_date <- as.Date(ref_date_str)
-        actual_dates <- ref_date + as.numeric(time_var)  # Add time_var as days
-      } else if(grepl("seconds", time_units)) {
+      # Get dates
+      terra_rast <- terra::rast(nc_file_path)
+      if (!is.na(terra::time(terra_rast[[1]]))) {
+        nc_data <- nc_open(nc_file_path)
+        time_var <- ncvar_get(nc_data, "time")
+        time_units <- ncatt_get(nc_data, "time", "units")$value
+        ref_date_str <- sub(".*since ", "", time_units)
+        unit <- strsplit(time_units, " ")[[1]][1]
+        
         ref_date <- as.POSIXct(ref_date_str, tz = "UTC")
-        actual_dates <- ref_date + dseconds(as.numeric(time_var))  # Add time_var as seconds
-      } else if(grepl("minutes", time_units)) {
-        ref_date <- as.POSIXct(ref_date_str, tz = "UTC")
-        actual_dates <- ref_date + dminutes(as.numeric(time_var))  # Add time_var as minutes
-      } else if(grepl("hours", time_units)) {
-        ref_date <- as.POSIXct(ref_date_str, tz = "UTC")
-        actual_dates <- ref_date + dhours(as.numeric(time_var))  # Add time_var as hours
+        actual_dates <- switch(unit,
+          "days" = ref_date + ddays(as.numeric(time_var)),
+          "seconds" = ref_date + dseconds(as.numeric(time_var)),
+          "minutes" = ref_date + dminutes(as.numeric(time_var)),
+          "hours" = ref_date + dhours(as.numeric(time_var)),
+          stop("Time units not recognized or supported")
+        )
+        nc_close(nc_data)
+      } else if (!is.na(names(terra_rast[[1]])) && !is.null(reference_date) && !is.null(file_frequency)) {
+        rast_filename <- names(terra_rast)
+        rast_filename_num <- as.numeric(gsub("[^0-9]", "", rast_filename))
+        actual_dates <- as.Date(reference_date) + rast_filename_num
       } else {
-        stop("Time units not recognized or supported")
+        stop("Time variable not found in the nc file")
       }
       
-      start_date <- min(actual_dates) 
-      end_date <- max(actual_dates)
-      nc_close(nc_data)
+      if (length(actual_dates) != length(clim_daily_mean)) {
+        warning("Mismatch between number of dates and data points. Using sequence.")
+        actual_dates <- seq(min(actual_dates), by = "day", length.out = length(clim_daily_mean))
+      }
 
-      dates <- seq(as.Date(start_date), as.Date(end_date), by="days")
-
-      # Add the dates to the results data frame
-      clim_df$date <- dates
-
-      # Bind the dataset and order variables
-      results_df <- rbind(results_df, clim_df)
-      results_df <- results_df |> select(date, Attribute, clim_daily_mean)
+      file_results[[i]] <- data.frame(
+        date = actual_dates,
+        Attribute = area_attribute,
+        clim_daily_mean = clim_daily_mean
+      )
     }
-    return(results_df)
+    
+    return(do.call(rbind, file_results))
   }
 
   # Process all NC files
-  for (nc_file in clim_files) {
-    results_df <- rbind(results_df, process_nc_file(nc_file))
-  }
-
+  results_list <- lapply(clim_files, process_nc_file)
+  
+  # Combine all results
+  results_df <- do.call(rbind, results_list)
+  
   return(results_df)
 }
-
-# Example
-# pacman::p_load(tidyverse, data.table, janitor, fst, beepr, openxlsx, lme4, broom, broom.mixed, googledrive, here)
-# pacman::p_load(sf, sp, raster, terra, tidyterra, ncdf4, rnaturalearth)
-# library(rnaturalearthdata)
-# rm(list = ls())
-
-# # Step-0: Point to folders that contain the raw WBGT and precip data ----
-# path_processed <- here("2-data", "2.2-processed-data")
-# here_tmax_wb_raw <- "Z:/Shared drives/Benmarhnia Lab/Arnab/common-datasets/climate-datasets/world-temp-wetbulb-max/test"
-
-
-# # head(df_dhs_psu_geo_sf)
-
-# # Step-2: load-function to extract climate data to DHS PSUs ----
-# source(here("1-scripts", "dummy.R"))
-
-# # Import India Shapefile
-# path_india_state <- "Z:/Shared drives/Benmarhnia Lab/Arnab/common-datasets/gis-datasets/nfhs-shape-files-from-abhishek/dist-shp-file-701/"
-# india_state_sf <- read_sf(here(path_india_state, "N5_state.shp"))
-
-
-# # Step-3: Run the function to extract climate data for each PSU ----
-# df_wbgt <- func_extract_clim_data_shp(path_nic_files = here_tmax_wb_raw, 
-#                                       sf_file = india_state_sf, 
-#                                       sf_file_admin = "STATE_NAME")
-
-
